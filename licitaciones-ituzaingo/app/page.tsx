c99b8605f07a1025da6f58d8ebfe193e636c2a80
unknown <deoromartinignacio@gmail.com>
2026-02-05 18:14:41 -0300

    se crea el proyecto

---
+++ licitaciones-ituzaingo/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Módulo de Licitaciones – Municipio de Ituzaingó
+      </h1>
+    </main>
+  );
+}
