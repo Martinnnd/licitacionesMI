940aff21b2497a893940014e5c32cb59509ca1a2
unknown <deoromartinignacio@gmail.com>
2026-02-07 17:32:59 -0300

    cambios

--- licitaciones-ituzaingo/app/page.tsx
+++ licitaciones-ituzaingo/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-semibold">
-        Módulo de Licitaciones – Municipio de Ituzaingó
-      </h1>
-    </main>
-  );
+  redirect("/licitaciones");
 }
